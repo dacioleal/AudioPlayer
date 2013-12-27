81af394818c8db21cea5c8c1963d36e75c576cb2
dacioleal <dacioleal@gmail.com>
2013-12-27 10:38:03 +0100

    Initial Commit

---
+++ AudioPlayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AudioPlayer
+//
+//  Created by Dacio Leal Rodriguez on 27/12/13.
+//  Copyright (c) 2013 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AudioPlayerTests/AudioPlayerTests.m
@@ -0,0 +1,34 @@
+//
+//  AudioPlayerTests.m
+//  AudioPlayerTests
+//
+//  Created by Dacio Leal Rodriguez on 27/12/13.
+//  Copyright (c) 2013 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AudioPlayerTests : XCTestCase
+
+@end
+
+@implementation AudioPlayerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
